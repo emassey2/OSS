a43bb1937a6431a9d3304d94735712c69631f71e
emassey <emassey2@wisc.edu>
2014-06-07 20:31:30 -0400

    attempting to fix untracked (tracked) files

--- project-src/uart.d
+++
@@ -1,9 +0,0 @@
-.\uart.o: UART.c
-.\uart.o: inc/UART.h
-.\uart.o: C:\Program Files (x86)\ARM\ARMCC\bin\..\include\stdint.h
-.\uart.o: C:\Program Files (x86)\ARM\ARMCC\bin\..\include\stdbool.h
-.\uart.o: C:\Program Files (x86)\ARM\ARMCC\bin\..\include\stdio.h
-.\uart.o: C:\Program Files (x86)\ARM\ARMCC\bin\..\include\errno.h
-.\uart.o: inc/board_config.h
-.\uart.o: inc/lm4f120h5qr.h
-.\uart.o: inc/hw_types.h
